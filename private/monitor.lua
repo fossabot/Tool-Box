8b7733266618104ba52ef2a47db9bb41b3035b62
5m1Ly <5pixeldevelopment@gmail.com>
2022-01-28 05:36:49 -0800

    updated the depricated MSG function to the new class

--- private/monitor.lua
+++ private/monitor.lua
@@ -2,7 +2,7 @@
 ACTIVE_SESSIONS = _POOL:CREATE()
 INACTIVE_SESSIONS = _POOL:CREATE()
 
-local function CONNECT_MONITOR(NAME, SET_KICK_REASON, DEFER)
+local function CONNECT_MONITOR(NAME, SET_KICK_REASON, DEFERRALS)
 
 	local SOURCE = tonumber(source)
 	local CFG = CONFIG
@@ -10,71 +10,37 @@ local function CONNECT_MONITOR(NAME, SET_KICK_REASON, DEFER)
 	-- check if session exists within pool for the client (note: added cus somehow the connecting event triggered twice)
 	if ACTIVE_SESSIONS.POOL[SOURCE] == nil then
 
-		-- a function te format the connection progress within a string
-		local function CREATE_PROGRESS_STRING(MSG, DOING)
-			local PROGRESS = (100 / #CFG.DEFERALS_TEXT) * MSG --TODO: look for better way to keep track of loading percentage
-			local VISUAL_PROGRESS = math.floor(PROGRESS / 5)
-			local FRONT_VISUAL = VISUAL_PROGRESS
-			local BACK_VISUAL = 20 - VISUAL_PROGRESS
-			local PROGRESS_STRING = ('[%s%s] '):format(
-				string.rep('/', FRONT_VISUAL),
-				string.rep('~', BACK_VISUAL)
-			)..PROGRESS
-			return PROGRESS_STRING
-		end
-	
-		-- send a message with the 
-		local function MSG(SRC, DEFER, MSG, DOING, ...)
-			local PROGRESS = CREATE_PROGRESS_STRING(MSG, DOING)
-			MSG = CFG.DEFERALS_TEXT[MSG]
-			MSG = MSG:format(...)
-			DEFER.update(MSG..'\n'..PROGRESS..'% '..DOING)
-		end
+        local msg = _messages:new(NAME, DEFERRALS)
+        Wait(10)
 
-		DEFER.defer()
+        DEBUG:LOG(msg)
 
-		-- let client know we are creating a session
-		MSG(SOURCE, DEFER, 1, "creating session", CONFIG.SERVER_NAME, NAME)
-
-		-- start a new session
-		local SESSION = _SESSION:START(SOURCE, NAME)
-
-		-- add the session to the active sessions pool
-		ACTIVE_SESSIONS:ADD_SESSION(SOURCE, SESSION)
+        msg:send() -- let client know we are creating a session
+		local SESSION = _SESSION:START(SOURCE, NAME) -- start a new session		
+		ACTIVE_SESSIONS:ADD_SESSION(SOURCE, SESSION) -- add the session to the active sessions pool
 		Wait(600)
 
-		-- let client know we are creating its data
-		MSG(SOURCE, DEFER, 2, "getting/adding data", CONFIG.SERVER_NAME, NAME)
-
-		-- format the session for the client
-		SESSION:SET_DATA()
+		msg:send() -- let client know we are creating its data
+		SESSION:SET_DATA() -- format the session for the client
 		Wait(900)
 
-		-- let client know we are update thier session in pool
-		MSG(SOURCE, DEFER, 3, "storing session", CONFIG.SERVER_NAME, NAME)
-
-		ACTIVE_SESSIONS:SET_SESSION(SOURCE, SESSION)
+        msg:send() -- let client know we are update thier session in pool
+		ACTIVE_SESSIONS:SET_SESSION(SOURCE, SESSION) -- initialize the session data
 		Wait(1000)
 
-		-- let client know we are now about to save thier data
-		MSG(SOURCE, DEFER, 4, "saving session data", CONFIG.SERVER_NAME, NAME)
-
-		-- save the session data
-		SESSION:SAVE_DATA()
+        msg:send() -- let client know we are now about to save thier data
+		SESSION:SAVE_DATA() -- save the session data
 		Wait(600)
 
 		if (CONFIG.WHITELISTED and SESSION.QUEUE.WHITELISTED) or not CONFIG.WHITELISTED then
-			-- let client know we are now ready and that they will join within a few secconds
-			MSG(SOURCE, DEFER, 5, "done.... joining within a few seconds", CONFIG.SERVER_NAME, NAME)
-			Wait(2000)
-			exports.connectqueue:SEND_TO_QUEUE(SOURCE, NAME, SET_KICK_REASON, DEFER)
-			-- DEFER.done()
+            msg:send() -- let client know we are now ready and that they will join within a few secconds
+			Wait(100000)
+			exports.connectqueue:SEND_TO_QUEUE(SOURCE, NAME, SET_KICK_REASON, DEFERRALS)
 		else
-			DEFER.done('Je moet op de whitelist staan om hier te kunnen spelen...')
+			DEFERRALS.done(CFG.DEFER_MSGS.NOT_ON_LIST)
 		end
 
 	end
-
 end
 
 local function JOIN_MONITOR(TEMP_SOURCE)
